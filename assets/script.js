193a94d5d5cae5c83b9d53ca8dda308fc0c9b9bc
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-01-26 14:03:55 +0100

    Premier commit

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+const slides = [
+  {
+    image: "slide1.jpg",
+    title: "La capoeira",
+    text: "Développer ma capacité d'improvisation."
+  },
+  {
+    image: "slide2.jpg",
+    title: "Le crossfit",
+    text: "Repousser mes limites."
+  },
+  {
+    image: "slide3.jpg",
+    title: "La batterie",
+    text: "Développer mon sens du rythme et ma créativité"
+  },
+  {
+    image: "slide4.jpg",
+    title: " Pratique d'un sport saisonnier :",
+    text: "Plongée sous-marine, surf et ski."
+  },
+];
+ // Elements
+  // Définir les éléments
+  const carrouselImage = document.querySelector('.carrouselMove')
+  const carrouselH3 = document.querySelector("#carrousel h3");
+  const carrouselp = document.querySelector("#carrousel p");
+  const dots = document.querySelectorAll(".dot");
+  console.log(carrouselImage)
+
+// Définir l'index initial
+let index = 0;
+
+// Slide suivante
+function slide(index) {
+  carrouselImage.style.backgroundImage = `url(./Images/slide/${slides[index].image})`;
+  carrouselH3.innerHTML = slides[index].title;
+  carrouselp.innerHTML = slides[index].text;
+  for(let i = 0 ; i < slides.length; i++){
+    dots[i].classList.remove("dot_selected")
+  }
+  dots[index].classList.add("dot_selected");
+}
+
+// Click Right
+const arrowRight = document.getElementById("arrow_right");
+arrowRight.addEventListener("click", () => {
+  index = (index + 1)%slides.length
+  slide(index);
+});
+
+// Click Left
+const arrowLeft = document.getElementById("arrow_left");
+arrowLeft.addEventListener("click", () => {
+  index = (index - 1 + slides.length)%slides.length
+  slide(index);
+});
